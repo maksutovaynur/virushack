42430861fd29063ee818d0c698efca3f2a0b4455
maksutovaynur <maksutov_aynur@mail.ru>
2020-05-02 23:23:01 +0300

    drugs

--- web/test.py
+++ web/test.py
@@ -1,13 +1,45 @@
 import streamlit as st
+import random, string
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
+w_complaints = st.text_input(
+    label="Please describe your symptops here:",
+    value="I have a headache", key="symptoms", type="default")
+
+
+@st.cache
+def get_symptoms(input_text):
+    return [j for j in [i.strip().lower() for i in input_text.split()] if len(j) > 4]
+
+
+symtoms = get_symptoms(w_complaints)
+
+w_symtoms = st.multiselect(
+    label='Select symptoms to find drug against',
+    options=symtoms,
+    default=symtoms)
+
+
+@st.cache
+def find_farmacies(symtoms_list):
+    return [f"{randomword(random.randint(9, 18)).capitalize()} <some drug against '{s}'>" for s in symtoms_list*2]
+
+
+farmacies = find_farmacies(w_symtoms)
+
+w_farmacies = st.multiselect(
+    label='Select drugs to buy',
+    options=farmacies,
+    default=farmacies)
+
+w_button = st.button(
+    label="Find a drugstore..."
 )
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+if w_button:
+    st.text("Вот тебе и список аптек")
+
+
