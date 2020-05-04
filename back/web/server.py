38ebd3847f2a03211975eb96fbda861e6f96d75c
aynur <maksutov_aynur@mail.ru>
2020-05-04 20:08:16 +0300

    moved back

---
+++ back/web/server.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Query
+from ..lib.funcs import get_disease_names
+
+app = FastAPI()
+
+
+@app.get("/diseases")
+def root(text: str = Query(...), max_len: int = Query(5)):
+    return {"diseases": get_disease_names(text, max_len=max_len)}
