f8471b9797fc59ad8f98dbdf444254176b4fdea4
aynur <maksutov_aynur@mail.ru>
2020-05-04 20:19:38 +0300

    move front

--- back/web/server.py
+++ back/web/server.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, Query
-from ..lib.funcs import get_disease_names
+from back.lib.funcs import get_disease_names_by_text, get_doctor_by_disease
 
 app = FastAPI()
 
 
 @app.get("/diseases")
 def root(text: str = Query(...), max_len: int = Query(5)):
-    return {"diseases": get_disease_names(text, max_len=max_len)}
+    return {
+        "result": [
+            {'disease': d, 'doctor': get_doctor_by_disease(disease_name=d)}
+            for d in get_disease_names_by_text(text, max_len=max_len)
+        ]
+    }
