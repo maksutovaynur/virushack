2367ca0c9693a07aee95aad3bbcd9f3693fe1252
aynur <maksutov_aynur@mail.ru>
2020-05-04 20:39:58 +0300

    ok

--- back/lib/funcs.py
+++ back/lib/funcs.py
@@ -9,15 +9,18 @@ def get_disease_names_by_text(text, max_len):
     return _get_disease_names_by_text(max_len)
 
 
-def _get_disease_names_by_text(max_len):
-    return [randomword(10).capitalize() for i in range(max_len)]
-
 
 def get_doctor_by_disease(disease_name):
     return _get_doctor_by_disease(disease_name)
+
+
+def get_treatments_by_disease(disease_name):
+    return []
 
 
 def _get_doctor_by_disease(disease_name):
     return f"Doctor against disease '{disease_name}'"
 
 
+def _get_disease_names_by_text(max_len):
+    return [randomword(10).capitalize() for i in range(max_len)]
