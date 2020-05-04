635b425a22a19c49c2c5b4e2c0819c0453b7dcd7
aynur <maksutov_aynur@mail.ru>
2020-05-04 22:05:32 +0300

    add anchor

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
-from back.lib.funcs import get_disease_names_by_text, get_doctor_by_disease
+from back.lib.funcs import get_disease_names_by_text, get_doctor_by_disease, get_treatments_by_disease
 
 app = FastAPI()
 
@@ -9,7 +9,11 @@ app = FastAPI()
 def root(text: str = Query(...), max_len: int = Query(5)):
     return {
         "result": [
-            {'disease': d, 'doctor': get_doctor_by_disease(disease_name=d)}
+            {
+                'disease': d,
+                'doctor': get_doctor_by_disease(disease_name=d),
+                'treatments': get_treatments_by_disease(d)
+            }
             for d in get_disease_names_by_text(text, max_len=max_len)
         ]
     }
