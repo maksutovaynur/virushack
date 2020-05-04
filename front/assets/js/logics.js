fc30cb89f079985e1936e19b8ffec60f69a53224
aynur <maksutov_aynur@mail.ru>
2020-05-04 21:27:22 +0300

    o]

--- front/assets/js/logics.js
+++ front/assets/js/logics.js
@@ -1,4 +1,26 @@
-function VIVA_find_function(event){
+function build_ul(items, parent_id){
+    diag = document.getElementById(parent_id);
+    ul = document.createElement('ul');
+    items.forEach(function (item) {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML += item;
+    });
+    diag.innerHTML = '';
+    diag.appendChild(ul);
+}
 
-    alert("Ok!")
+
+
+async function VIVA_find_function(event){
+    text = document.getElementById('complaint-text').value;
+    let response = await fetch(
+        `/api/diseases?text=${text}`
+    );
+    if (!response.ok) return;
+    let result = (await response.json())["result"];
+
+    build_ul(result.map(x => x['disease']), "diagnosis");
+    build_ul(result.map(x => x['doctor']), "chatroom")
+
 }
