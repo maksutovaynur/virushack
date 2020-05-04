1e7a636af6a1d7d99b36b4daad5b8294fefc13b9
aynur <maksutov_aynur@mail.ru>
2020-05-04 20:23:19 +0300

    deleted

--- lib/funcs.py
+++
@@ -1,9 +0,0 @@
-import random, string
-
-def randomword(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
-
-
-def get_disease_names(text, max_len):
-    return [randomword(10).capitalize() for i in range(max_len)]
