3aab7be31d73c9aabfb43ee3af3b685cfad53860
aynur <maksutov_aynur@mail.ru>
2020-05-04 20:19:38 +0300

    move front

--- back/lib/funcs.py
+++ back/lib/funcs.py
@@ -5,5 +5,19 @@ def randomword(length):
     return ''.join(random.choice(letters) for i in range(length))
 
 
-def get_disease_names(text, max_len):
+def get_disease_names_by_text(text, max_len):
+    return _get_disease_names_by_text(max_len)
+
+
+def _get_disease_names_by_text(max_len):
     return [randomword(10).capitalize() for i in range(max_len)]
+
+
+def get_doctor_by_disease(disease_name):
+    return _get_doctor_by_disease(disease_name)
+
+
+def _get_doctor_by_disease(disease_name):
+    return f"Doctor against disease '{disease_name}'"
+
+
