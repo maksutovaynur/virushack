167f891398c47ca170bb1719be11187c0c7b90da
maksutovaynur <maksutov_aynur@mail.ru>
2020-05-05 22:04:34 +0300

    redirect

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
 from back.lib.funcs import get_disease_names_by_text, get_doctors_by_disease, get_treatments_by_disease
 
 app = FastAPI()
 
 
 @app.get("/api/diseases")
-def root(text: str = Query(...), max_diseases: int = Query(3), max_doctors: int = Query(2), max_treatments: int = Query(2)):
+def api_endpoint_diseases(text: str = Query(...), max_diseases: int = Query(3), max_doctors: int = Query(2), max_treatments: int = Query(2)):
     result = {
         "result": [
             {
@@ -19,5 +20,11 @@ def root(text: str = Query(...), max_diseases: int = Query(3), max_doctors: int 
     }
     print("Result:", result)
     return result
+
+
+@app.get("/")
+def root():
+    return RedirectResponse("/index.html")
+
 
 app.mount("/", StaticFiles(directory="front"))
